90eb2640cdad7998feef5e3814e937ea1d3a31d5
Jack-Hickisch <jackhickisch@gmail.com>
2020-04-13 15:04:52 -0600

    made code to test efficiency

--- Sort.java
+++ Sort.java
@@ -2,8 +2,9 @@ import java.util.*;
 
 public class Sort
 {
-    public static void selectionSort(ArrayList<String> list)
+    public static int selectionSort(ArrayList<String> list)
     {
+        int checksCount = 0;
         int startIndex = 0;
         int smallestIndex = 0;
 
@@ -15,16 +16,20 @@ public class Sort
                 {
                     smallestIndex = j;
                 }
+                checksCount++;
             }
 
             Collections.swap(list, i, smallestIndex);
             startIndex++;
             smallestIndex = startIndex;
         }
+
+        return checksCount;
     }
 
-    public static void insertionSort(ArrayList<String> list)
+    public static int insertionSort(ArrayList<String> list)
     {
+        int checksCount = 0;
         int elementsOn = 2;
         boolean inOrder = false;
 
@@ -40,15 +45,18 @@ public class Sort
                         Collections.swap(list, i, i + 1);
                         inOrder = false;
                     }
+                    checksCount++;
                 }
             }
             elementsOn++;
             inOrder = false;           
         }
+        return checksCount;
     }
 
-    public static void bubbleSort(ArrayList<String> list)
+    public static int bubbleSort(ArrayList<String> list)
     {
+        int checksCount = 0;
         int swapCount = 1; // cant be 0 or else loop wont start
 
         while (swapCount != 0)
@@ -61,8 +69,10 @@ public class Sort
                     Collections.swap(list, i, i + 1);
                     swapCount++;
                 }
+                checksCount++;
             }
         }
+        return checksCount;
     }
 
     public static boolean isBeforeNeedle(String test, String needle) // 2 words
@@ -70,7 +80,7 @@ public class Sort
         boolean before = false;
         boolean done = false;
 
-        for (int i = 0; i < test.length() - 1; i++)
+        for (int i = 0; i < test.length(); i++)
         {
             if (!done && positionOfLetter(test.substring(i, i + 1), needle.substring(i, i + 1)) == 1)
             {
@@ -110,30 +120,56 @@ public class Sort
 
     public static void main(String[] args)
     {
-        ArrayList<String> fruits0 = new ArrayList<String>(Arrays.asList("mango", "banana", "strawberry", "cherry", "lime", "pear", "apple", "watermelon", "lemon", "blueberry", "orange"));
-        ArrayList<String> fruits1 = new ArrayList<String>(Arrays.asList("mango", "banana", "strawberry", "cherry", "lime", "pear", "apple", "watermelon", "lemon", "blueberry", "orange"));
-        ArrayList<String> fruits2 = new ArrayList<String>(Arrays.asList("mango", "banana", "strawberry", "cherry", "lime", "pear", "apple", "watermelon", "lemon", "blueberry", "orange"));
+        // ArrayList<String> fruits0 = new ArrayList<String>(Arrays.asList("mango", "banana", "strawberry", "cherry", "lime", "pear", "apple", "watermelon", "lemon", "blueberry", "orange"));
+        // ArrayList<String> fruits1 = new ArrayList<String>(Arrays.asList("mango", "banana", "strawberry", "cherry", "lime", "pear", "apple", "watermelon", "lemon", "blueberry", "orange"));
+        // ArrayList<String> fruits2 = new ArrayList<String>(Arrays.asList("mango", "banana", "strawberry", "cherry", "lime", "pear", "apple", "watermelon", "lemon", "blueberry", "orange"));
 
-        selectionSort(fruits0);
-        System.out.println(fruits0.toString());
+        // selectionSort(fruits0);
+        // System.out.println(fruits0.toString());
 
-        insertionSort(fruits1);
-        System.out.println(fruits1.toString());
+        // insertionSort(fruits1);
+        // System.out.println(fruits1.toString());
 
-        bubbleSort(fruits2);
-        System.out.println(fruits2.toString());
+        // bubbleSort(fruits2);
+        // System.out.println(fruits2.toString());
 
-        ArrayList<String> fruits20 = new ArrayList<String>(Arrays.asList("pear", "banana", "raspberry", "apple", "lime", "mango", "apricot", "watermelon", "lemon", "blueberry", "blueberry"));
-        ArrayList<String> fruits21 = new ArrayList<String>(Arrays.asList("pear", "banana", "raspberry", "apple", "lime", "mango", "apricot", "watermelon", "lemon", "blueberry", "blueberry"));
-        ArrayList<String> fruits22 = new ArrayList<String>(Arrays.asList("pear", "banana", "raspberry", "apple", "lime", "mango", "apricot", "watermelon", "lemon", "blueberry", "blueberry"));
+        // ArrayList<String> fruits20 = new ArrayList<String>(Arrays.asList("pear", "banana", "raspberry", "apple", "lime", "mango", "apricot", "watermelon", "lemon", "blueberry", "blueberry"));
+        // ArrayList<String> fruits21 = new ArrayList<String>(Arrays.asList("pear", "banana", "raspberry", "apple", "lime", "mango", "apricot", "watermelon", "lemon", "blueberry", "blueberry"));
+        // ArrayList<String> fruits22 = new ArrayList<String>(Arrays.asList("pear", "banana", "raspberry", "apple", "lime", "mango", "apricot", "watermelon", "lemon", "blueberry", "blueberry"));
 
-        selectionSort(fruits20);
-        System.out.println(fruits20.toString());
+        // selectionSort(fruits20);
+        // System.out.println(fruits20.toString());
 
-        insertionSort(fruits21);
-        System.out.println(fruits21.toString());
+        // insertionSort(fruits21);
+        // System.out.println(fruits21.toString());
 
-        bubbleSort(fruits22);
-        System.out.println(fruits22.toString());
+        // bubbleSort(fruits22);
+        // System.out.println(fruits22.toString());
+
+        String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        ArrayList<String> testForNums = new ArrayList<String>();
+
+        for (int i = 0; i < alphabet.length; i++)
+        {
+            for(int j = 0; j < alphabet.length; j++)
+            {
+                testForNums.add(alphabet[i] + alphabet[j]);
+            }
+        }
+
+        Collections.shuffle(testForNums);
+
+        System.out.println("selectionSort checks:    " + selectionSort(testForNums));
+        System.out.println("selectionSort list size: " + testForNums.size());
+
+        Collections.shuffle(testForNums);
+
+        // System.out.println("insertionSort checks:    " + insertionSort(testForNums));
+        // System.out.println("insertionSort list size: " + testForNums.size());
+
+        // Collections.shuffle(testForNums);
+
+        System.out.println("bubbleSort checks:    " + bubbleSort(testForNums));
+        System.out.println("bubbleSort list size: " + testForNums.size());
     }
 }
